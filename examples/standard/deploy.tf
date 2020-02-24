2d6896f41e72d1e7dea510ccdeb159215027ca4f
ravi <ravi.periketi@fxinnovation.com>
2020-02-24 10:48:47 -0500

    test

--- examples/standard/deploy.tf
+++ examples/standard/deploy.tf
@@ -7,42 +7,42 @@ module "hub_resource_group" {
   location = var.location
 }
 
-module "spoke_resource_group" {
-  providers = {
-    azurerm.dst = azurerm.spoke
-  }
-  source   = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-resource-group.git?ref=0.2.0"
-  name     = "spoke-resource-group"
-  location = var.location
-}
+# module "spoke_resource_group" {
+#   providers = {
+#     azurerm.dst = azurerm.spoke
+#   }
+#   source   = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-resource-group.git?ref=0.2.0"
+#   name     = "spoke-resource-group"
+#   location = var.location
+# }
 
-module "hub_vnet" {
-  providers = {
-    azurerm.src = azurerm.hub
-  }
-  source              = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
-  resource_group_name = module.hub_resource_group.name
-  vnet_name           = var.hub_vnet_name
-  vnet_address_space  = var.hub_vnet_address_space
-}
+# module "hub_vnet" {
+#   providers = {
+#     azurerm.src = azurerm.hub
+#   }
+#   source              = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
+#   resource_group_name = module.hub_resource_group.name
+#   vnet_name           = var.hub_vnet_name
+#   vnet_address_space  = var.hub_vnet_address_space
+# }
 
-module "spoke_vnet" {
-  providers = {
-    azurerm.src = azurerm.spoke
-  }
-  source              = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
-  resource_group_name = module.spoke_resource_group.name
-  vnet_name           = var.spoke_vnet_name
-  vnet_address_space  = var.spoke_vnet_address_space
-}
+# module "spoke_vnet" {
+#   providers = {
+#     azurerm.src = azurerm.spoke
+#   }
+#   source              = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
+#   resource_group_name = module.spoke_resource_group.name
+#   vnet_name           = var.spoke_vnet_name
+#   vnet_address_space  = var.spoke_vnet_address_space
+# }
 
-module "hub-spoke-peering" {
-  providers = {
-    azurerm.src = azurerm.hub
-    azurerm.dst = azurerm.spoke
-  }
-  source = "../.."
+# module "hub-spoke-peering" {
+#   providers = {
+#     azurerm.src = azurerm.hub
+#     azurerm.dst = azurerm.spoke
+#   }
+#   source = "../.."
 
-  vnet_src_id = module.hub_vnet.virtual_network_id
-  vnet_dst_id = module.spoke_vnet.virtual_network_id
-}
+#   vnet_src_id = module.hub_vnet.virtual_network_id
+#   vnet_dst_id = module.spoke_vnet.virtual_network_id
+# }
