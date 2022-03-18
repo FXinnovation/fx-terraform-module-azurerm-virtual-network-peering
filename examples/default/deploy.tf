add986e1115b54f0e5c66967a3220f4f18e2474a
Stephen Rouleau <stephen.rouleau@fxinnovation.com>
2022-03-18 14:08:22 -0400

    Convert dazzlingwrench Gitea URL to FXI Github Enterprise URL

--- examples/default/deploy.tf
+++ examples/default/deploy.tf
@@ -8,7 +8,7 @@ module "src_resource_group" {
   providers = {
     azurerm = azurerm.source
   }
-  source   = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-resource-group.git?ref=0.2.0"
+  source   = "git::https://github.com/FXinnovation/fx-terraform-module-azurerm-resource-group.git?ref=0.2.0"
   name     = "source-resource-group-${random_string.this.result}"
   location = "canadaeast"
 }
@@ -17,7 +17,7 @@ module "dst_resource_group" {
   providers = {
     azurerm = azurerm.destination
   }
-  source   = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-resource-group.git?ref=0.2.0"
+  source   = "git::https://github.com/FXinnovation/fx-terraform-module-azurerm-resource-group.git?ref=0.2.0"
   name     = "destination-resource-group-${random_string.this.result}"
   location = "canadaeast"
 }
@@ -26,7 +26,7 @@ module "src_vnet" {
   providers = {
     azurerm = azurerm.source
   }
-  source              = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
+  source              = "git::https://github.com/FXinnovation/fx-terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
   resource_group_name = module.src_resource_group.name
   vnet_name           = "hub-vnet-${random_string.this.result}"
   vnet_address_space  = ["10.1.0.0/24"]
@@ -36,7 +36,7 @@ module "dst_vnet" {
   providers = {
     azurerm = azurerm.destination
   }
-  source              = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
+  source              = "git::https://github.com/FXinnovation/fx-terraform-module-azurerm-virtualnetwork.git?ref=0.2.1"
   resource_group_name = module.dst_resource_group.name
   vnet_name           = "spoke-vnet-${random_string.this.result}"
   vnet_address_space  = ["10.2.0.0/24"]
